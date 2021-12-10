4afe2c5e8df53bbb0f1858efa4f8dcc33df763c9
zgorzal <zgorzalek.piotr@gmail.com>
2021-12-10 21:32:45 +0100

    tech commit

--- src/main/java/pl/zgorzal/charity_2_be/app/SecurityConfig.java
+++ src/main/java/pl/zgorzal/charity_2_be/app/SecurityConfig.java
@@ -57,6 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        http.cors().configurationSource(corsConfigurationSource());
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().antMatchers("/login", "/refresh").permitAll();
         http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean(), secret));
